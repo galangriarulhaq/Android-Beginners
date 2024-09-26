3b8251aeef985fdbb597375ce8b16b6e7be47417
galangriarulhaq <galangrz337@gmail.com>
2024-09-27 00:30:02 +0700

    Set Up for About Page

---
+++ app/src/main/java/com/example/myproject/AboutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
