af54c85be527e2f7e1afef7b1361079df40ad5b8
galangriarulhaq <galangrz337@gmail.com>
2024-09-28 23:15:22 +0700

    Create a banner and Add it to the About Page

--- app/src/main/java/com/example/myproject/SplashActivity.kt
+++ app/src/main/java/com/example/myproject/SplashActivity.kt
@@ -17,6 +17,6 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 3000)
+        }, 1000)
     }
 }
